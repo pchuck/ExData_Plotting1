a10accf1a377312d27ccd02af376aaf5d55ca8a2
patrick charles <pchuck@users.noreply.github.com>
2015-03-06 00:14:40 -0700

    fixed legend colors on plot3

--- plot3.R
+++ plot3.R
@@ -13,13 +13,13 @@ feb2power = subset(power, Date == "1/2/2007" | Date == "2/2/2007")
 feb2power$Global_active_power = as.numeric(feb2power$Global_active_power)
 feb2power$Date = strptime(paste(feb2power$Date, feb2power$Time), format="%d/%m/%Y %H:%M:%S")
 
-# create the graphic
+## create the graphic
 generatePlot <- function(feb2power) {
     with(feb2power, plot(Date, Sub_metering_1, ylab="Energy sub metering", xlab="", type="n"))
     with(feb2power, lines(Date, Sub_metering_1, col="black"))
     with(feb2power, lines(Date, Sub_metering_2, col="red"))
     with(feb2power, lines(Date, Sub_metering_3, col="blue"))
-    legend("topright", col = c("black", "blue", "red"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1, 1, 1))
+    legend("topright", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = c(1, 1, 1))
 }
 
 ## plot to the screen
